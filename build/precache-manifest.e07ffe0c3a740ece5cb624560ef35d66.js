92b4c2ee85111bdb5a553a49b6acde6a677367cb
marco mariscal <marco.a.mariscal@gmail.com>
2020-07-30 18:34:18 -0400

    updating

---
+++ build/precache-manifest.e07ffe0c3a740ece5cb624560ef35d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430492f00b927b19a5d5619191521ad8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07eb8f70adf8afffdd20",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "04581f54d54736931784",
+    "url": "/static/css/main.04294940.chunk.css"
+  },
+  {
+    "revision": "07eb8f70adf8afffdd20",
+    "url": "/static/js/2.dad3b906.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.dad3b906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04581f54d54736931784",
+    "url": "/static/js/main.2156d41d.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
