ebe1b4fb088e88f29efc4c8d0b06916cc7c8203e
marco mariscal <marco.a.mariscal@gmail.com>
2020-08-04 20:40:46 -0400

    updating image in companies

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CompanyCard.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBuilding } from "@fortawesome/free-solid-svg-icons";
 import "./FontAwesome.css";
 
 const CompanyCard = ({ item }) => {
+  const [imgValid, setImgValid] = useState(true);
+
   return (
     <div className="CompanyCard card text-center">
       <div className="card-body">
         <h6 className="card-title d-flex justify-content-between mb-4">
           <span className="text-capitalize">{item.name}</span>
-          {/* {!item.logo_url ? (
+          {imgValid && (
+            <img
+              src={item.logo_url}
+              alt={`${item.name}} Logo`}
+              onError={setImgValid(false)}
+            />
+          )}
+          {!imgValid && (
             <FontAwesomeIcon icon={faBuilding} size="2x" className="building" />
-          ) : (
-            <img src={item.logo_url} alt={`${item.name}} Logo`} onError={() => <FontAwesomeIcon icon={faBuilding} size="2x" className="building"/>}/>
-          )} */}
-            <img src={item.logo_url} alt={`${item.name}} Logo`} onError={() => <FontAwesomeIcon icon={faBuilding} size="2x" className="building"/>}/>
+          )}
         </h6>
         <p className="card-text text-left">{item.description}</p>
       </div>
